5226284e8c0e366d5006b37d7f12afe251cac2d6
Austin Burgard <awburgard@gmail.com>
2019-08-08 11:17:49 -0500

    adds bootstrap-vue and grid setup for questions

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Question Box</h1>
+  <div class="qustion-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        Some Question
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of Answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
