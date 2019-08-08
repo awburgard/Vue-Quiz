925db739c0c92f1230638076705d073b7d449557
Austin Burgard <awburgard@gmail.com>
2019-08-08 11:09:40 -0500

    sets up frontend

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled> <b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: </b-nav-item>
+    </b-nav>
+  </div>
 </template>
