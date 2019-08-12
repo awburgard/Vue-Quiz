f4e37df7c4fa1fd5542c60f8f18382d65be94dee
Austin Burgard <awburgard@gmail.com>
2019-08-12 12:14:17 -0500

    updates server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-let port = process.env.PORT || 5000;
+const app = express();
+app.use(express.static("dist"));
+let port = process.env.PORT || 8080;
 app.listen(port);
 console.log("server started " + port);
