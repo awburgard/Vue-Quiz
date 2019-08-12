228c2d4d3950c63b3e4774781b5788b680be4083
Austin Burgard <awburgard@gmail.com>
2019-08-12 11:51:24 -0500

    adds necessary pkgs for deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-
-// Serve static files
-app.use(express.static("build"));
-
-// App Set //
-const PORT = process.env.PORT || 8080;
-
-/** Listen * */
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+const serveStatic = require("serve-static");
+const path = require("path");
+app = express();
+app.use(serveStatic(path.join(__dirname, "dist")));
+const port = process.env.PORT || 80;
+app.listen(port);
