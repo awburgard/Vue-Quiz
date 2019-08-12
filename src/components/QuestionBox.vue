534d9ff9b81ffb50a47e9245f3fb9b1f0d0e629c
Austin Burgard <awburgard@gmail.com>
2019-08-12 10:50:14 -0500

    adds lodash and updates npm

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,7 +6,12 @@
       <hr class="my-4" />
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{ answer }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
@@ -21,23 +26,58 @@ export default {
     currentQuestion: Object,
     next: Function
   },
+  data() {
+    return {
+      selectedIndex: null
+    };
+  },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
     }
+  },
+  watch: {
+    currentQuestion(){
+      this.selectedIndex = null;
+      this.shuffleAnswers()
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+    shuffleAnswers(){
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+    }
   }
 };
 </script>
 
 <style scoped>
-.list-group{
+.list-group {
   margin-bottom: 15px;
 }
 
-.btn{
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+
+.btn {
   margin: 0 5px;
 }
 
-</style>
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.incorrect {
+  background-color: red;
+}
+</style> 
