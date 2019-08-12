89f4ab43b15182a8989b87b734a9771e9377a11b
Austin Burgard <awburgard@gmail.com>
2019-08-12 10:28:01 -0500

    adds answers to dom

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="qustion-box-container">
     <b-jumbotron>
-      <template slot="lead">
-        {{ currentQuestion.question }}
-      </template>
+      <template slot="lead">{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
 
-      <p>
-        List of Answers
-      </p>
+      <b-list-group>
+        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{ answer }}</b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -21,7 +19,25 @@
 export default {
   props: {
     currentQuestion: Object,
-    next: Function,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.list-group{
+  margin-bottom: 15px;
+}
+
+.btn{
+  margin: 0 5px;
+}
+
+</style>
