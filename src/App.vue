c041dd5bdedcc97f55702a5f82f4837afc1a6303
Austin Burgard <awburgard@gmail.com>
2019-08-12 11:21:36 -0500

    adds conditional colors to answers, counter to answers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
-          <QuestionBox 
+          <QuestionBox
             v-if="questions.length"
             :currentQuestion="questions[index]"
             :next="next"
+            :increment="increment"
           />
         </b-col>
       </b-row>
@@ -18,34 +20,41 @@
 <script>
 import Header from "./components/Header.vue";
 import QuestionBox from "./components/QuestionBox.vue";
-
 export default {
   name: "app",
   components: {
     Header,
     QuestionBox
   },
-  data(){
+  data() {
     return {
       questions: [],
       index: 0,
-    }
+      numCorrect: 0,
+      numTotal: 0
+    };
   },
   methods: {
     next() {
-      this.index++
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++;
     }
   },
-  mounted: function () {
-    fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple', {
-      method: 'get'
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
+      method: "get"
     })
-    .then((response) => {
-      return response.json()
-    })
-    .then((jsonData)=> {
-      this.questions = jsonData.results
-    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
   }
 };
 </script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,83 +1,126 @@
 <template>
-  <div class="qustion-box-container">
+  <div class="question-box-container">
     <b-jumbotron>
-      <template slot="lead">{{ currentQuestion.question }}</template>
+      <template slot="lead">
+        {{ currentQuestion.question }}
+      </template>
 
       <hr class="my-4" />
 
       <b-list-group>
         <b-list-group-item
-          v-for="(answer, index) in answers"
+          v-for="(answer, index) in shuffledAnswers"
           :key="index"
           @click.prevent="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : '']"
-        >{{ answer }}</b-list-group-item>
+          :class="answerClass(index)"
+        >
+          {{ answer }}
+        </b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button @click="next" variant="success" href="#">Next</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >
+        Submit
+      </b-button>
+      <b-button @click="next" variant="success">
+        Next
+      </b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
   },
-  data() {
+  data: function() {
     return {
-      selectedIndex: null
-    };
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    }
   },
   computed: {
     answers() {
-      let answers = [...this.currentQuestion.incorrect_answers];
-      answers.push(this.currentQuestion.correct_answer);
-      return answers;
+      // this function is no longer used in finished code
+      // it is replaced by the watch function below and the
+      // shuffleAnswers method
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
     }
   },
   watch: {
-    currentQuestion(){
-      this.selectedIndex = null;
-      this.shuffleAnswers()
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null
+        this.answered = false
+        this.shuffleAnswers()
+      }
     }
   },
   methods: {
     selectAnswer(index) {
-      this.selectedIndex = index;
+      this.selectedIndex = index
     },
-    shuffleAnswers(){
+    submitAnswer() {
+      let isCorrect = false
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true
+      }
+      this.answered = true
+      this.increment(isCorrect)
+    },
+    shuffleAnswers() {
       let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    },
+    answerClass(index) {
+      let answerClass = ''
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = 'selected'
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = 'correct'
+      } else if (this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = 'incorrect'
+      }
+      return answerClass
     }
   }
-};
+}
 </script>
 
 <style scoped>
 .list-group {
   margin-bottom: 15px;
 }
-
 .list-group-item:hover {
-  background: #eee;
+  background: #EEE;
   cursor: pointer;
 }
-
 .btn {
   margin: 0 5px;
 }
-
 .selected {
   background-color: lightblue;
 }
-
 .correct {
   background-color: lightgreen;
 }
-
 .incorrect {
   background-color: red;
 }
-</style> 
+</style>
